2c9acba085b99989cbd4d70d48b7d665006a7426
Nicholas Myers <neekgreen@users.noreply.github.com>
2017-08-10 07:29:07 -0500

    MvcCoreBuilder support; start unit test setup

---
+++ tests/PaginableCollections.AspNetCore.Tests/ValuesControllerTests.cs
@@ -0,0 +1,18 @@
+﻿namespace PaginableCollections.AspNetCore.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public static void ShouldDoSomething()
+        {
+
+        }
+    }
+}
