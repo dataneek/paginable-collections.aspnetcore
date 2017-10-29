d381f9ec9f90410dd8b3b2b984ef28cb919bdaa7
Jonathan Madison <jonathan@Adtrav-Mac02.local>
2017-10-28 19:13:02 -0500

    added test projects for each paginable header format

---
+++ tests/PaginableCollections.AspNetCore.IntegrationTests.Expanded/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,32 @@
+namespace PaginableCollections.AspNetCore.IntegrationTests.Expanded
+{
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.TestHost;
+    using Xunit;
+
+    public class ValuesControllerTests
+    {
+        private readonly TestServer testServer;
+        private readonly HttpClient httpClient;
+
+        public ValuesControllerTests()
+        {
+            testServer =
+                new TestServer(
+                    new WebHostBuilder().UseStartup<Startup>());
+
+            httpClient = testServer.CreateClient();
+        }
+
+        private async Task<HttpResponseMessage> GetResult(int pageNumber, int itemCountPerPage)
+        {
+            var request = $"/api/values/{pageNumber}/{itemCountPerPage}";
+            var response = await httpClient.GetAsync(request);
+
+            return response;
+        }
+    }
+}
