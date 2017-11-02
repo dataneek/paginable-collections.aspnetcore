2d1c6f54c65f1f58b5d2da16c1a5b91bf8901961
Nicholas Myers <neekgreen@users.noreply.github.com>
2017-11-02 11:53:11 -0500

    custom name scheme

--- src/PaginableCollections.AspNetCore/HeaderFormatOptions.cs
+++ src/PaginableCollections.AspNetCore/HeaderFormatOptions.cs
@@ -1,11 +1,14 @@
 ﻿namespace PaginableCollections.AspNetCore
 {
+    using System;
+
     public class HeaderFormatOptions
     {
         public HeaderFormatOptions() { }
 
 
         public HeaderFormat HeaderFormat { get; set; } = HeaderFormat.Condensed;
+        public INamingSchemeBuilder NamingSchemeBuilder { get; set; } = new NamingSchemeBuilder();
 
 
         public HeaderFormatOptions UseCondensed()
@@ -18,6 +21,11 @@
         {
             HeaderFormat = HeaderFormat.Expanded;
             return this;
+        }
+
+        public void WithNamingScheme(Action<INamingSchemeBuilder> builder)
+        {
+            builder(this.NamingSchemeBuilder);
         }
     }
 }
